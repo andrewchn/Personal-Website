18f38c7903109cf077895b313ac4b15b174343cb
Andrew <ajchin01@gmail.com>
2022-11-09 23:08:45 -0500

    image?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
+import {ProfilePic} from './components/ProfilePic.js'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           Welcome to My Website!
       </header>
+      <ProfilePic/>
     </div>
   );
 }
